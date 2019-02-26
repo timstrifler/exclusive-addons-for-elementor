430f83b5d88835ed019b3dd9307c8d02b243db9e
All <All@Atiques-MacBook-Air.local>
2019-02-26 22:10:16 +0600

    Added Some controls on countdown

--- elements/countdown-timer/countdown-timer.php
+++ elements/countdown-timer/countdown-timer.php
@@ -73,7 +73,7 @@ class Exad_Countdown_Timer extends Widget_Base {
 
 
 		/*
-		 * Team Members Styling Section
+		 * Countdown Timer Styling Section
 		 */
 
 		$this->start_controls_section(
@@ -91,8 +91,9 @@ class Exad_Countdown_Timer extends Widget_Base {
 				'type' => Controls_Manager::SELECT,
 				'default' => 'style-1',
 				'options' => [
-					'style-1' 		=> esc_html__( 'Style 1', 		'exclusive-addons-elementor' ),
-					'style-2' 	=> esc_html__( 'Style 2', 	'exclusive-addons-elementor' ),
+					'style-1' 	=> esc_html__( 'Style 1', 'exclusive-addons-elementor' ),
+					'style-2' 	=> esc_html__( 'Style 2', 'exclusive-addons-elementor' ),
+					'style-3' 	=> esc_html__( 'Style 3', 'exclusive-addons-elementor' ),
 				],
 			]
 		);
@@ -143,121 +144,197 @@ class Exad_Countdown_Timer extends Widget_Base {
 			]
 		);
 
-		$this->add_control(
-			'exad_countdown_border_radius',
+		$this->add_group_control(
+			Group_Control_Box_Shadow::get_type(),
 			[
-				'label' => esc_html__( 'Border Radius', 'exclusive-addons-elementor' ),
-				'type' => Controls_Manager::COLOR,
-				'default' => '',
-				'selectors' => [
-					'{{WRAPPER}} .exad-countdown.style-1 .exad-countdown-container' => 'background-color: {{VALUE}};',
-				],
+				'name' => 'box_shadow',
+				'label' => __( 'Box Shadow', 'exclusive-addons-elementor' ),
+				'selector' => '{{WRAPPER}} .exad-countdown-container',
 				'condition' => [
 					'exad_countdown_preset' => 'style-1',
 				],
 			]
 		);
+
+		// $this->add_control(
+		// 	'exad_countdown_border_radius',
+		// 	[
+		// 		'label' => esc_html__( 'Border Radius', 'exclusive-addons-elementor' ),
+		// 		'type' => Controls_Manager::COLOR,
+		// 		'default' => '',
+		// 		'selectors' => [
+		// 			'{{WRAPPER}} .exad-countdown.style-1 .exad-countdown-container' => 'background-color: {{VALUE}};',
+		// 		],
+		// 		'condition' => [
+		// 			'exad_countdown_preset' => 'style-1',
+		// 		],
+		// 	]
+		// );
 
 		$this->end_controls_section();
-
-		/*
-		* Team Member Image Styling
-		*/
+		
+		// Count Styles
 
 		$this->start_controls_section(
-			'exad_section_countdown_image_styles',
+			'exad_section_countdown_styles_count',
 			[
-				'label' => esc_html__( 'Team Member Image Style', 'exclusive-addons-elementor' ),
+				'label' => esc_html__( 'Count Style', 'exclusive-addons-elementor' ),
 				'tab' => Controls_Manager::TAB_STYLE
 			]
-		);		
-
-		$this->add_responsive_control(
-			'exad_countdown_image_width',
-			[
-				'label' => esc_html__( 'Image Width', 'exclusive-addons-elementor' ),
-				'type' => Controls_Manager::SLIDER,
-				'default' => [
-					'size' => 100,
-					'unit' => '%',
-				],
-				'range' => [
-					'%' => [
-						'min' => 0,
-						'max' => 100,
-					],
-					'px' => [
-						'min' => 0,
-						'max' => 1000,
-					],
-				],
-				'size_units' => [ '%', 'px' ],
-				'selectors' => [
-					'{{WRAPPER}} .exad-team-item figure img' => 'width:{{SIZE}}{{UNIT}};',
-				],
-			]
 		);
 
-
-		$this->add_responsive_control(
-			'exad_countdown_image_margin',
-			[
-				'label' => esc_html__( 'Margin', 'exclusive-addons-elementor' ),
-				'type' => Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', '%' ],
-				'selectors' => [
-					'{{WRAPPER}} .exad-team-item figure img' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-				],
-			]
-		);
-
-		$this->add_responsive_control(
-			'exad_countdown_image_padding',
-			[
-				'label' => esc_html__( 'Padding', 'exclusive-addons-elementor' ),
-				'type' => Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', '%', 'em' ],
-				'selectors' => [
-					'{{WRAPPER}} .exad-team-item figure img' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-				],
-			]
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+				[
+						'name' => 'count_typography',
+						'selector' => '{{WRAPPER}} .exad-countdown-count',
+				]
 		);
 
 		$this->add_control(
-			'exad_countdown_bg_svg',
+			'exad_progress_bar_count_color',
 			[
-				'label' => esc_html__( 'Avatar Background Color', 'exclusive-addons-elementor' ),
+				'label' => __( 'Color', 'exclusive-addons-elementor' ),
 				'type' => Controls_Manager::COLOR,
-				'default' => 'rgba(255,255,255,0.8)',
+				'default' => '#000',
 				'selectors' => [
-					'{{WRAPPER}} .exad-team-image svg.exad-member-img-bg g' => 'fill: {{VALUE}};',
-				],
-				'condition' => [
-					'exad_countdown_preset' => 'style-1',
+					'{{WRAPPER}} .exad-countdown-count' => 'color: {{VALUE}};',
 				],
 			]
 		);
 		
-		$this->add_group_control(
-			Group_Control_Border::get_type(),
+		$this->end_controls_section();
+		
+		// Title Styles
+
+		$this->start_controls_section(
+			'exad_section_countdown_styles_title',
 			[
-				'name' => 'exad_countdown_image_border',
-				'label' => esc_html__( 'Border', 'exclusive-addons-elementor' ),
-				'selector' => '{{WRAPPER}} .exad-team-item figure img',
+				'label' => esc_html__( 'Title Style', 'exclusive-addons-elementor' ),
+				'tab' => Controls_Manager::TAB_STYLE
 			]
+		);
+
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+				[
+						'name' => 'title_typography',
+						'selector' => '{{WRAPPER}} .exad-countdown-title',
+				]
 		);
 
 		$this->add_control(
-			'exad_countdown_image_rounded',
+			'exad_progress_bar_title_color',
 			[
-				'label' => esc_html__( 'Rounded Avatar?', 'exclusive-addons-elementor' ),
-				'type' => Controls_Manager::SWITCHER,
-				'return_value' => 'team-avatar-rounded',
-				'default' => '',
+				'label' => __( 'Color', 'exclusive-addons-elementor' ),
+				'type' => Controls_Manager::COLOR,
+				'default' => '#000',
+				'selectors' => [
+					'{{WRAPPER}} .exad-countdown-title' => 'color: {{VALUE}};',
+				],
 			]
 		);
-
+		
 		$this->end_controls_section();
+
+		// /*
+		// * Team Member Image Styling
+		// */
+
+		// $this->start_controls_section(
+		// 	'exad_section_countdown_image_styles',
+		// 	[
+		// 		'label' => esc_html__( 'Team Member Image Style', 'exclusive-addons-elementor' ),
+		// 		'tab' => Controls_Manager::TAB_STYLE
+		// 	]
+		// );		
+
+		// $this->add_responsive_control(
+		// 	'exad_countdown_image_width',
+		// 	[
+		// 		'label' => esc_html__( 'Image Width', 'exclusive-addons-elementor' ),
+		// 		'type' => Controls_Manager::SLIDER,
+		// 		'default' => [
+		// 			'size' => 100,
+		// 			'unit' => '%',
+		// 		],
+		// 		'range' => [
+		// 			'%' => [
+		// 				'min' => 0,
+		// 				'max' => 100,
+		// 			],
+		// 			'px' => [
+		// 				'min' => 0,
+		// 				'max' => 1000,
+		// 			],
+		// 		],
+		// 		'size_units' => [ '%', 'px' ],
+		// 		'selectors' => [
+		// 			'{{WRAPPER}} .exad-team-item figure img' => 'width:{{SIZE}}{{UNIT}};',
+		// 		],
+		// 	]
+		// );
+
+
+		// $this->add_responsive_control(
+		// 	'exad_countdown_image_margin',
+		// 	[
+		// 		'label' => esc_html__( 'Margin', 'exclusive-addons-elementor' ),
+		// 		'type' => Controls_Manager::DIMENSIONS,
+		// 		'size_units' => [ 'px', '%' ],
+		// 		'selectors' => [
+		// 			'{{WRAPPER}} .exad-team-item figure img' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+		// 		],
+		// 	]
+		// );
+
+		// $this->add_responsive_control(
+		// 	'exad_countdown_image_padding',
+		// 	[
+		// 		'label' => esc_html__( 'Padding', 'exclusive-addons-elementor' ),
+		// 		'type' => Controls_Manager::DIMENSIONS,
+		// 		'size_units' => [ 'px', '%', 'em' ],
+		// 		'selectors' => [
+		// 			'{{WRAPPER}} .exad-team-item figure img' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+		// 		],
+		// 	]
+		// );
+
+		// $this->add_control(
+		// 	'exad_countdown_bg_svg',
+		// 	[
+		// 		'label' => esc_html__( 'Avatar Background Color', 'exclusive-addons-elementor' ),
+		// 		'type' => Controls_Manager::COLOR,
+		// 		'default' => 'rgba(255,255,255,0.8)',
+		// 		'selectors' => [
+		// 			'{{WRAPPER}} .exad-team-image svg.exad-member-img-bg g' => 'fill: {{VALUE}};',
+		// 		],
+		// 		'condition' => [
+		// 			'exad_countdown_preset' => 'style-1',
+		// 		],
+		// 	]
+		// );
+		
+		// $this->add_group_control(
+		// 	Group_Control_Border::get_type(),
+		// 	[
+		// 		'name' => 'exad_countdown_image_border',
+		// 		'label' => esc_html__( 'Border', 'exclusive-addons-elementor' ),
+		// 		'selector' => '{{WRAPPER}} .exad-team-item figure img',
+		// 	]
+		// );
+
+		// $this->add_control(
+		// 	'exad_countdown_image_rounded',
+		// 	[
+		// 		'label' => esc_html__( 'Rounded Avatar?', 'exclusive-addons-elementor' ),
+		// 		'type' => Controls_Manager::SWITCHER,
+		// 		'return_value' => 'team-avatar-rounded',
+		// 		'default' => '',
+		// 	]
+		// );
+
+		// $this->end_controls_section();
 
 	}
 
@@ -273,8 +350,8 @@ class Exad_Countdown_Timer extends Widget_Base {
 	protected function _content_template() {
 		?>
 		<div id="exad-countdown-timer" class="exad-countdown-content-container">
-        	<div class="exad-countdown {{ settings.exad_countdown_preset }}" data-day="Days" data-minutes="Minutes" data-hours="Hours" data-seconds="Seconds" data-countdown="{{ settings.exad_countdown_time }}"></div>
-    	</div>
+				<div class="exad-countdown {{ settings.exad_countdown_preset }}" data-day="Days" data-minutes="Minutes" data-hours="Hours" data-seconds="Seconds" data-countdown="{{ settings.exad_countdown_time }}"></div>
+		</div>
 		<?php
 	}
 
