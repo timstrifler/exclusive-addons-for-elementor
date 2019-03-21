8c2403c1953306047a9eaf25e0dfab1a15738c5b
Muntasir Mahmud Aumio <secretspeed.aumz@gmail.com>
2019-03-21 21:13:31 +0600

    Testimonial and team carousel responsive slick

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -47,7 +47,7 @@
 				  }
 				},
 				{
-				  breakpoint: 768,
+				  breakpoint: 576,
 				  settings: {
 					slidesToShow: 1,
 				  }
@@ -90,10 +90,16 @@
 			rows: 0,
 			responsive: [
 				{
-				  breakpoint: 768,
-				  settings: {
-					slidesToShow: 1,
-				  }
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 2,
+					},
+				},	
+				{
+					breakpoint: 576,
+					settings: {
+						slidesToShow: 1,
+					}
 				}
 			],
 	    });	
