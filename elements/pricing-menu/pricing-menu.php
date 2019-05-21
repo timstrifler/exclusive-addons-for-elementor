aa39d0eee23fb1b882235c421ecbc1918647c287
fahim100 <fahimahmmed100@gmail.com>
2019-05-21 19:23:23 +0600

    Price menu review needed.

--- elements/pricing-menu/pricing-menu.php
+++ elements/pricing-menu/pricing-menu.php
@@ -82,6 +82,36 @@ class Exad_Pricing_Menu extends Widget_Base {
         'default' => esc_html__( 'Asparagus, hens egg, toasted sunflower seeds, Spenwood cheese', 'exclusive-addons-elementor' ),
       ]
     );
+    $price_menu_repeater->add_control(
+        'exad_pricing_menu_enable_link',
+        [
+            'label' => __( 'Enable Order Button', 'exclusive-addons-elementor' ),
+            'type' => Controls_Manager::SWITCHER,
+            'label_on' => __( 'Show', 'exclusive-addons-elementor' ),
+            'label_off' => __( 'Hide', 'exclusive-addons-elementor' ),
+            'return_value' => 'yes',
+            'default' => 'no',
+        ]
+    );
+    $price_menu_repeater->add_control(
+        'exad_pricing_menu_action_text',
+        [
+            'label' => esc_html__('Order Action', 'exclusive-addons-elementor'),
+            'type' => Controls_manager::TEXT,
+            'default' => 'Order Now',
+            'condition' => [
+                'exad_pricing_menu_enable_link' => 'yes',
+            ],
+        ]
+    );
+    $price_menu_repeater->add_control(
+        'exad_pricing_menu_price',
+        [
+            'label' => __( 'Price', 'exclusive-addons-elementor' ),
+            'type' => Controls_Manager::NUMBER,
+            'default' => 14,
+        ]
+    );
     $this->add_control(
         'pricing_menu_repeater',
         [
@@ -97,7 +127,6 @@ class Exad_Pricing_Menu extends Widget_Base {
                 ],
                 [
                     'exad_pricing_menu_title' => __( 'List #3', 'exclusive-addons-elementor' ),
-                    
                 ],
                 [
                     'exad_pricing_menu_title' => __( 'List #4', 'exclusive-addons-elementor' ),
@@ -107,15 +136,177 @@ class Exad_Pricing_Menu extends Widget_Base {
     );
 
     $this->end_controls_section();
+    /**
+     * Pricing menu style
+     */
+    $this->start_controls_section(
+        'exad_section_pricing_menu_style',
+      [
+        'label' => esc_html__( 'Preset', 'exclusive-addons-elementor' ),
+        'tab' => Controls_Manager::TAB_STYLE,
+      ]
+    );
+    $this->add_control(
+		'exad_pricing_menu_preset',
+		[
+			'label' => esc_html__( 'Presets', 'exclusive-addons-elementor' ),
+			'type' => Controls_Manager::SELECT,
+			'default' => 'one',
+			'separator' => 'before',
+			'options' => [
+				'one' => esc_html__( 'One', 'exclusive-addons-elementor' ),
+				'two' => esc_html__( 'Two', 'exclusive-addons-elementor' ), 
+				'three' => esc_html__( 'Three', 'exclusive-addons-elementor' ), 
+				'four' => esc_html__( 'Four', 'exclusive-addons-elementor' ),
+				'five' => esc_html__( 'Five', 'exclusive-addons-elementor' ),
+				'six' => esc_html__( 'Six', 'exclusive-addons-elementor' ),
+			]
+		]
+    );
+    $this->end_controls_section();
+    /**
+     * Pricing menu Title style
+     */
+    $this->start_controls_section(
+        'exad_pricing_menu_title',
+      [
+        'label' => esc_html__( 'Title', 'exclusive-addons-elementor' ),
+        'tab' => Controls_Manager::TAB_STYLE,
+      ]
+    );
+    $this->start_controls_tabs( 'exad_pricing_menu_title_color' );
+        $this->start_controls_tab( 'exad_pricing_menu_title_color_control', [ 'label' => esc_html__( 'Normal', 'exclusive-addons-elementor' ) ] );
+            $this->add_control(
+                'exad_pricing_menu_title_color_normal',
+                [
+                    'label' => esc_html__( 'Title Color', 'exclusive-addons-elementor' ),
+                    'type' => Controls_Manager::COLOR,
+                    'default' => '#0A1724',
+                    'selectors' => [
+                        '{{WRAPPER}} .exad-pricing-list-item .exad-pricing-list-item-content .exad-pricing-list-item-content-title' => 'color: {{VALUE}};',
+                    ],
+                ]
+            );
+        $this->end_controls_tab();
+        $this->start_controls_tab( 'exad_pricing_menu_title_color_hover_control', [ 'label' => esc_html__( 'Hover', 'exclusive-addons-elementor' ) ] );
+            $this->add_control(
+                'exad_pricing_menu_title_color_hover',
+                [
+                    'label' => esc_html__( 'Title Color', 'exclusive-addons-elementor' ),
+                    'type' => Controls_Manager::COLOR,
+                    'default' => '#dea728',
+                    'selectors' => [
+                        '{{WRAPPER}} .exad-pricing-list-item .exad-pricing-list-item-content .exad-pricing-list-item-content-title:hover' => 'color: {{VALUE}};',
+                    ],
+                ]
+            );
+        $this->end_controls_tab();
+    $this->end_controls_tabs();
+    $this->add_group_control(
+        Group_Control_Typography::get_type(),
+        [
+            'name' => 'exad_pricing_menu_title_typography',
+            'label' => __( 'Title Typography', 'exclusive-addons-elementor' ),
+            'selector' => '{{WRAPPER}} .exad-pricing-list-item .exad-pricing-list-item-content .exad-pricing-list-item-content-title',
+        ]
+    );
+    $this->end_controls_section();
+    /**
+     * Pricing menu Description style
+     */
+    $this->start_controls_section(
+        'exad_pricing_menu_description',
+      [
+        'label' => esc_html__( 'Description', 'exclusive-addons-elementor' ),
+        'tab' => Controls_Manager::TAB_STYLE,
+      ]
+    );
+    $this->add_control(
+        'exad_pricing_menu_description_color',
+        [
+            'label' => esc_html__( 'Description Color', 'exclusive-addons-elementor' ),
+            'type' => Controls_Manager::COLOR,
+            'default' => '#8b8e93',
+            'selectors' => [
+                '{{WRAPPER}} .exad-pricing-list-item .exad-pricing-list-item-content .exad-pricing-list-item-content-description' => 'color: {{VALUE}};',
+            ],
+        ]
+    );
+        
+    $this->add_group_control(
+        Group_Control_Typography::get_type(),
+        [
+            'name' => 'exad_pricing_menu_description_typography',
+            'label' => __( 'Description Typography', 'exclusive-addons-elementor' ),
+            'selector' => '{{WRAPPER}} .exad-pricing-list-item .exad-pricing-list-item-content .exad-pricing-list-item-content-description',
+        ]
+    );
+    $this->end_controls_section();
+
+    /**
+     * Pricing menu Price style
+     */
+    $this->start_controls_section(
+        'exad_pricing_menu_price',
+      [
+        'label' => esc_html__( 'Price', 'exclusive-addons-elementor' ),
+        'tab' => Controls_Manager::TAB_STYLE,
+      ]
+    );
+    $this->add_control(
+        'exad_pricing_menu_price_color',
+        [
+            'label' => esc_html__( 'Price Color', 'exclusive-addons-elementor' ),
+            'type' => Controls_Manager::COLOR,
+            'default' => '#f43d6b',
+            'selectors' => [
+                '{{WRAPPER}} .exad-pricing-list-item .exad-pricing-list-item-price' => 'color: {{VALUE}};',
+            ],
+        ]
+    );
+        
+    $this->add_group_control(
+        Group_Control_Typography::get_type(),
+        [
+            'name' => 'exad_pricing_menu_price_typography',
+            'label' => __( 'Description Typography', 'exclusive-addons-elementor' ),
+            'selector' => '{{WRAPPER}} .exad-pricing-list-item .exad-pricing-list-item-price',
+        ]
+    );
+    $this->end_controls_section();
+
+    /**
+     * Pricing menu Saperator style
+     */
+    $this->start_controls_section(
+        'exad_pricing_menu_separetor',
+      [
+        'label' => esc_html__( 'Seperator', 'exclusive-addons-elementor' ),
+        'tab' => Controls_Manager::TAB_STYLE,
+      ]
+    );
+    $this->add_control(
+        'exad_pricing_menu_seperator_color',
+        [
+            'label' => esc_html__( 'Seperator Color', 'exclusive-addons-elementor' ),
+            'type' => Controls_Manager::COLOR,
+            'default' => '#e3e3e3',
+            'selectors' => [
+                '{{WRAPPER}} .exad-pricing-list-item' => 'border-bottom-color: {{VALUE}};'
+            ],
+        ]
+    );
+    $this->end_controls_section();
 	}
 
 	protected function render() {
 
         
         $settings = $this->get_settings_for_display();
+        $pricing_menu_preset = $settings['exad_pricing_menu_preset'];
 
         ?>
-            <div class="exad-pricing-list one">
+            <div class="exad-pricing-list <?php echo $pricing_menu_preset ?>">
                 <div class="exad-pricing-list-wrapper">
                 <?php foreach ( $settings['pricing_menu_repeater'] as $list ) : 
 
@@ -130,15 +321,28 @@ class Exad_Pricing_Menu extends Widget_Base {
                             </div>
                         <?php } ?>
                         <div class="exad-pricing-list-item-content">
-                            <a href="#" class="exad-pricing-list-item-content-title">
-                                <?php echo $list['exad_pricing_menu_title'] ?>
-                            </a>
+                            <?php if( $settings['exad_pricing_menu_preset'] === 'six' ) { ?>
+                                <div class="exad-pricing-list-item-content-element">
+                                    <a href="#" class="exad-pricing-list-item-content-title">
+                                        <?php echo $list['exad_pricing_menu_title'] ?>
+                                    </a>
+                                    <span class="exad-pricing-list-item-content-conntector"></span>
+                                </div>
+                            <?php } 
+                            else { ?>
+                                <a href="#" class="exad-pricing-list-item-content-title">
+                                    <?php echo $list['exad_pricing_menu_title'] ?>
+                                </a>
+                            <?php } ?>
                             <p class="exad-pricing-list-item-content-description">
                                 <?php echo $list['exad_pricing_menu_description'] ?>
                             </p>
+                            <?php if( $list['exad_pricing_menu_enable_link'] === 'yes' ) { ?>
+                                <a href="#" class="exad-pricing-list-item-content-action"><?php echo $list['exad_pricing_menu_action_text'] ?></a>
+                            <?php } ?>
                         </div>
                         <div class="exad-pricing-list-item-price">
-                            <span>$14.5</span>
+                            <span>$<?php echo $list['exad_pricing_menu_price'] ?></span>
                         </div>
                     </div>
                  <?php endforeach; ?>
