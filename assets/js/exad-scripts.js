485eb7a94aabf902f4b068b6e162ba611a3fef68
Muntasir Mahmud Aumio <secretspeed.aumz@gmail.com>
2019-05-28 17:57:11 +0600

    Filterable Gallery One setup

--- assets/js/exad-scripts.js
+++ assets/js/exad-scripts.js
@@ -523,7 +523,63 @@
 			$(this).css({ "left" : leftPos, "top" : topPos });
 		});
 		
-	};
+    };
+    
+    // Filterable gallery
+    var FilterableGallery = function( $scope, $ ) {
+
+        var $galleryWrapper = $scope.find('#exad-gallery-one').eq(0),
+            $galleryElement = $galleryWrapper.find('.exad-gallery-element'),
+            $galleryFilter = $galleryWrapper.find('#filters'),
+            $galleryMenu = $galleryWrapper.find('.exad-gallery-menu');
+
+        // filter functions
+        var filterFns = {
+            // show if number is greater than 50
+            numberGreaterThan50: function() {
+            var number = $(this).find('.number').text();
+            return parseInt( number, 10 ) > 50;
+            },
+            // show if name ends with -ium
+            ium: function() {
+            var name = $(this).find('.name').text();
+            return name.match( /ium$/ );
+            }
+        };    
+
+        var $gallery = $galleryElement.isotope({
+            itemSelector: '#exad-gallery-one .exad-gallery-item',
+            layoutMode: 'fitRows',
+            getSortData: {
+              name: '.name',
+              symbol: '.symbol',
+              number: '.number parseInt',
+              category: '[data-category]',
+              weight: function( itemElem ) {
+                var weight = $( itemElem ).find('.weight').text();
+                return parseFloat( weight.replace( /[\(\)]/g, '') );
+              }
+            }
+        });
+
+        // bind filter button click
+        $galleryFilter.on( 'click', 'button', function() {
+            var filterValue = $( this ).attr('data-filter');
+            // use filterFn if matches value
+            filterValue = filterFns[ filterValue ] || filterValue;
+            $gallery.isotope({ filter: filterValue });
+        });
+
+        // change is-checked class on buttons
+        $galleryMenu.each( function( i, buttonGroup ) {
+            var $buttonGroup = $( buttonGroup );
+            $buttonGroup.on( 'click', 'button', function() {
+                $buttonGroup.find('.is-checked').removeClass('is-checked');
+                $( this ).addClass('is-checked');
+            });
+        });
+
+    };
 
 
 	$(window).on('elementor/frontend/init', function () {
@@ -543,7 +599,8 @@
 		elementorFrontend.hooks.addAction('frontend/element_ready/exad-image-comparison.default', ImageComparison);
 		elementorFrontend.hooks.addAction('frontend/element_ready/exad-counter.default', CounterUp);
 		elementorFrontend.hooks.addAction('frontend/element_ready/exad-logo-carousel.default', LogoCarousel);
-		elementorFrontend.hooks.addAction('frontend/element_ready/exad-modal-popup.default', ModalPopup);
+        elementorFrontend.hooks.addAction('frontend/element_ready/exad-modal-popup.default', ModalPopup);
+        elementorFrontend.hooks.addAction('frontend/element_ready/exad-filterable-gallery.default', FilterableGallery);
 		//elementorFrontend.hooks.addAction('frontend/element_ready/exad-image-hotspot.default', ImageHotspot);
     });
 
