6213468459a134c20b4f5f0954853eb3895b35d9
Muntasir Mahmud Aumio <secretspeed.aumz@gmail.com>
2019-07-16 17:01:17 +0600

    debounce fix

--- assets/js/exad-editor-script.js
+++ assets/js/exad-editor-script.js
@@ -2,9 +2,9 @@
     'use strict';
 
     elementor.on('panel:init', function() {
-        $('#elementor-panel-elements-search-input').on('keyup', _.debounce(function() {
+        $('#elementor-panel-elements-search-input').on('keyup', function() {
             $('#elementor-panel-elements').find('.exad-element-icon').parents('.elementor-element').addClass('exad-element-wrapper');
-        }, 100));
+        });
     });
 
 }(jQuery, elementor));
