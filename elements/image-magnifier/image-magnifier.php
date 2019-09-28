8aabe9fc5318d521c0751702795fb820b5163bea
fahim100 <fahimahmmed100@gmail.com>
2019-09-28 16:09:52 +0600

    Added some option in image magnifier.

--- elements/image-magnifier/image-magnifier.php
+++ elements/image-magnifier/image-magnifier.php
@@ -50,9 +50,6 @@ class Exad_Image_Magnifier extends Widget_Base {
         [
             'name' => 'thumbnail',
             'default' => 'full',
-            'condition' => [
-                'exad_comparison_image_one[url]!' => '',
-            ],
         ]
     );
     
@@ -64,131 +61,147 @@ class Exad_Image_Magnifier extends Widget_Base {
     * image Comparison Style
     */
     $this->start_controls_section(
-        'exad_section_image_comparision_styles_presets',
+        'exad_section_image_magnefic_container',
         [
-            'label' => esc_html__( 'General', 'exclusive-addons-elementor' ),
+            'label' => esc_html__( 'Container', 'exclusive-addons-elementor' ),
             'tab' => Controls_Manager::TAB_STYLE
         ]
     );
-    
+
+    $this->add_control(
+        'exa_image_magnefic_container_image_width',
+        [
+            'label' => __( 'Width', 'exclusive-addons-elementor' ),
+            'type' => Controls_Manager::SLIDER,
+            'size_units' => [ 'px', '%' ],
+            'range' => [
+                'px' => [
+                    'min' => 0,
+                    'max' => 1000,
+                ],
+                '%' => [
+                    'min' => 0,
+                    'max' => 100,
+                ],
+            ],
+            'default' => [
+                'unit' => '%',
+                'size' => 100,
+            ],
+            'selectors' => [
+                '{{WRAPPER}} .exad-magnify-small' => 'width: {{SIZE}}{{UNIT}};',
+            ],
+        ]
+    );
 
     $this->add_group_control(
         Group_Control_Border::get_type(),
         [
-            'name' => 'exad_img_comparison_border',
-            'selector' => '{{WRAPPER}} .exad-image-comparision .exad-image-comparision-element',
+            'name' => 'exa_image_magnefic_container_image_border',
+            'label' => __( 'Border', 'exclusive-addons-elementor' ),
+            'selector' => '{{WRAPPER}} .exad-magnify-small',
         ]
     );
 
-
-    $this->add_control(
-        'exad_img_comparison_border_radius',
-        [
-            'label'       => __( 'Border Radius', 'exclusive-addons-elementor' ),
-            'type'        => Controls_Manager::DIMENSIONS,
-            'size_units'  => [ 'px', '%' ],
-            'default'     => [
-                'top' => '',
-                'right' => '',
-                'bottom' => '',
-                'left' => '',
-                'isLinked' => true,
-            ],
-            'selectors'   => [
-                '{{WRAPPER}} .exad-image-comparision .exad-image-comparision-element' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-            ],
-        ]
-    );
-
-    
-    $this->add_control(
-        'exad_image_magnifier_handle_type',
-        [
-            'label' => esc_html__( 'Handle Bar Type', 'exclusive-addons-elementor' ),
-            'type' => Controls_Manager::SELECT,
-            'default' => 'vertical',
-            'options' => [
-                'vertical' => esc_html__( 'Horizontal', 'exclusive-addons-elementor' ),
-                'horizontal' => esc_html__( 'Vertical', 'exclusive-addons-elementor' ),
-            ],
-        ]
-    );
-
-    $this->add_control(
-        'exad_image_magnifier_slider_handle',
-        [
-            'label' => esc_html__( 'Handle Style', 'exclusive-addons-elementor' ),
-            'type' => Controls_Manager::SELECT,
-            'default' => 'handle-style-1',
-            'options' => [
-                'handle-style-1' => esc_html__( 'Style 1', 'exclusive-addons-elementor' ),
-                'handle-style-2' => esc_html__( 'Style 2', 'exclusive-addons-elementor' ),
-            ],
-        ]
-    );
-
-    $this->add_control(
-		'exad_no_overlay',
-		[
-            'label'             => esc_html__( 'Disable Overlay', 'exclusive-addons-elementor' ),
-            'type'              => Controls_Manager::SWITCHER,
-            'default'           => 'true',
-            'label_on'          => __( 'On', 'exclusive-addons-elementor' ),
-            'label_off'         => __( 'Off', 'exclusive-addons-elementor' ),
-            'return_value'      => 'true',
-		]
-    );
-
-    $this->add_control(
-        'exad_before_label',
-        [
-            'label' => esc_html__( 'Overlay Before Text', 'exclusive-addons-elementor' ),
-            'type'  => Controls_Manager::TEXT,
-            'condition' => [
-                'exad_no_overlay' => ''
-            ]
-        ]
-    );
-
-    $this->add_control(
-        'exad_after_label',
-        [
-            'label' => esc_html__( 'Overlay After Text', 'exclusive-addons-elementor' ),
-            'type'  => Controls_Manager::TEXT,
-            'condition' => [
-                'exad_no_overlay' => ''
-            ]
-        ]
-    );
-    
-    $this->add_control(
-		'exad_default_offset_pct',
-        [
-			'label' => esc_html__( 'Handle Bar Position', 'exclusive-addons-elementor' ),
-            'type'  => Controls_Manager::SELECT,
-            'default' => '0.5',
-            'options' => [
-                    '0.0' => __( '0', 'exclusive-addons-elementor' ),
-                    '0.1' => __( '1', 'exclusive-addons-elementor' ),
-					'0.2' => __( '2', 'exclusive-addons-elementor' ),
-					'0.3' => __( '3', 'exclusive-addons-elementor' ),
-					'0.4' => __( '4', 'exclusive-addons-elementor' ),
-					'0.5' => __( '5', 'exclusive-addons-elementor' ),
-					'0.6' => __( '6', 'exclusive-addons-elementor' ),
-					'0.7' => __( '7', 'exclusive-addons-elementor' ),
-					'0.8' => __( '8', 'exclusive-addons-elementor' ),
-					'0.9' => __( '9', 'exclusive-addons-elementor' ),
-					'1.0' => __( '10', 'exclusive-addons-elementor' ),
-                ]
-			]
-	);
-    
     $this->add_group_control(
         Group_Control_Box_Shadow::get_type(),
         [
-            'name'     => 'exad_image_magnifier_box_shadow',
-            'label'    => __( 'Box Shadow', 'exclusive-addons-elementor' ),
-            'selector' => '{{WRAPPER}} .exad-image-comparision .exad-image-comparision-element'
+            'name' => 'exa_image_magnefic_container_image_box_shadow',
+            'label' => __( 'Box Shadow', 'exclusive-addons-elementor' ),
+            'selector' => '{{WRAPPER}} .exad-magnify-small',
+        ]
+    );
+    
+    $this->end_controls_section();
+
+    /*
+    * image Comparison Style
+    */
+    $this->start_controls_section(
+        'exad_section_image_magnefic_glass_style',
+        [
+            'label' => esc_html__( 'Magnific Glass', 'exclusive-addons-elementor' ),
+            'tab' => Controls_Manager::TAB_STYLE
+        ]
+    );
+
+    $this->add_control(
+        'exad_image_magnefic_glass_height',
+        [
+            'label' => __( 'Height', 'exclusive-addons-elementor' ),
+            'type' => Controls_Manager::SLIDER,
+            'size_units' => [ 'px' ],
+            'range' => [
+                'px' => [
+                    'min' => 0,
+                    'max' => 1000,
+                ],
+            ],
+            'default' => [
+                'unit' => 'px',
+                'size' => 175,
+            ],
+            'selectors' => [
+                '{{WRAPPER}} .exad-magnify-large' => 'height: {{SIZE}}{{UNIT}};',
+            ],
+        ]
+    );
+
+    $this->add_control(
+        'exad_image_magnefic_glass_width',
+        [
+            'label' => __( 'Width', 'exclusive-addons-elementor' ),
+            'type' => Controls_Manager::SLIDER,
+            'size_units' => [ 'px' ],
+            'range' => [
+                'px' => [
+                    'min' => 0,
+                    'max' => 1000,
+                ],
+            ],
+            'default' => [
+                'unit' => 'px',
+                'size' => 175,
+            ],
+            'selectors' => [
+                '{{WRAPPER}} .exad-magnify-large' => 'width: {{SIZE}}{{UNIT}};',
+            ],
+        ]
+    );
+
+    $this->add_group_control(
+        Group_Control_Border::get_type(),
+        [
+            'name' => 'exad_image_magnefic_glass_border',
+            'label' => __( 'Border', 'exclusive-addons-elementor' ),
+            'selector' => '{{WRAPPER}} .exad-magnify-large',
+        ]
+    );
+
+    $this->add_control(
+        'exad_image_magnefic_glass_radius',
+        [
+            'label' => __( 'Border Radius', 'exclusive-addons-elementor' ),
+            'type' => Controls_Manager::DIMENSIONS,
+            'size_units' => [ 'px', '%' ],
+            'default' => [
+                'top' => '0',
+                'right' => '0',
+                'bottom' => '0',
+                'left' => '0',
+            ],
+            'selectors' => [
+                '{{WRAPPER}} .exad-magnify-large' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+            ],
+        ]
+    );
+
+    $this->add_group_control(
+        Group_Control_Box_Shadow::get_type(),
+        [
+            'name' => 'exad_image_magnefic_glass_box_shadow',
+            'label' => __( 'Box Shadow', 'exclusive-addons-elementor' ),
+            'selector' => '{{WRAPPER}} .exad-magnify-large',
         ]
     );
     
@@ -200,13 +213,13 @@ class Exad_Image_Magnifier extends Widget_Base {
 	protected function render() {
 
     $settings = $this->get_settings_for_display();
-    $comparison_image_one = $this->get_settings_for_display( 'exad_comparison_image_one' );
-	$comparison_image_url_one_src = Group_Control_Image_Size::get_attachment_image_src( $comparison_image_one['id'], 'thumbnail', $settings );
+    $magnify_image = $this->get_settings_for_display( 'exad_magnify_image' );
+	$magnify_image_src_url = Group_Control_Image_Size::get_attachment_image_src( $magnify_image['id'], 'thumbnail', $settings );
 
-		if( empty( $comparison_image_url_one_src ) ) {
-			$comparison_image_url_one = $comparison_image_one['url']; 
+		if( empty( $magnify_image_src_url ) ) {
+			$magnify_image_url = $magnify_image['url']; 
 		} else { 
-			$comparison_image_url_one = $comparison_image_url_one_src;
+			$magnify_image_url = $magnify_image_src_url;
 		}
 		
 		$this->add_render_attribute( 'exad_image_magnifier_wrapper', [
@@ -224,7 +237,7 @@ class Exad_Image_Magnifier extends Widget_Base {
     <!-- Lets make a simple image magnifier -->
     <div class="exad-image-magnify">
         <div class="exad-magnify-large"></div>
-        <img class="exad-magnify-small" src="<?php echo esc_url( $comparison_image_url_one ); ?>" />
+        <img class="exad-magnify-small" src="<?php echo esc_url( $magnify_image_url ); ?>" />
     </div>
 
     <?php
