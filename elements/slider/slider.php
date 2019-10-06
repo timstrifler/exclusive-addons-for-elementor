6b4f3b87b5e3fae192de7ab7035b009fd8b47090
zabid_devscred <zabid.devscred@gmail.com>
2019-10-06 21:40:12 +0600

    slider.

--- elements/slider/slider.php
+++ elements/slider/slider.php
@@ -275,7 +275,7 @@ class ExclusiveSliderItem extends Widget_Base {
                 'label'         => __( 'Title', 'nivo-slider-elementor' ),
                 'description'   => __( 'Select title animation style for this slide only.', 'nivo-slider-elementor' ),
                 'type'          => Controls_Manager::SELECT,
-                'default'       => 'slideInDown',
+                'default'       => '',
                 'options'       => [
                     ''              => __('None', 'nivo-slider-elementor'),
                     'slideInUp'     => __('slideInUp', 'nivo-slider-elementor'),
@@ -1656,13 +1656,9 @@ class ExclusiveSliderItem extends Widget_Base {
 						echo '<div class="exad-slide-bg"></div>';
 						echo '<div class="exad-slide-inner">';
 							echo '<div class="exad-slide-content">';
-								// echo $each_slide['exad_slider_title'] ? '<h2 class="wow animated '.esc_attr($each_title_animation).'" data-wow-duration=".5s" data-wow-delay=".5s">'.esc_html($each_slide['exad_slider_title']).'</h2>' : '';
+								echo $each_slide['exad_slider_title'] ? '<h2 data-animation-in="fadeInDown" data-delay-in=".4" data-duration-in=".4" data-animation-out="fadeOutDown" data-delay-out="3" data-duration-out=".5">'.esc_html($each_slide['exad_slider_title']).'</h2>' : '';
 
-// echo $each_slide['exad_slider_title'] ? '<h2 data-animation-in="fadeIn" data-delay-in="1" data-duration-in="1" data-animation-out="fadeInDown" data-delay-out="1" data-duration-out="1">'.esc_html($each_slide['exad_slider_title']).'</h2>' : '';
-?>
-                            <h2 data-animation-in="fadeInDown" data-delay-in=".4" data-duration-in=".4" data-animation-out="fadeOutUp" data-delay-out="3" data-duration-out=".5">Registration</h2>
-                                <?php
-								// echo $each_slide['exad_slider_details'] ? '<p class="wow animated slideInUp" data-wow-duration="1s" data-wow-delay=".5s">'.wp_kses_post($each_slide['exad_slider_details']).'</p>' : '';
+								echo $each_slide['exad_slider_details'] ? '<p data-animation-in="fadeInDown" data-delay-in=".4" data-duration-in=".4" data-animation-out="fadeOutDown" data-delay-out="3" data-duration-out=".5">'.wp_kses_post($each_slide['exad_slider_details']).'</p>' : '';
 
 								if ( ! empty( $each_slide['exad_slider_button_text'] ) ) :
 								    if ( $each_slide['exad_slider_button_url']['url'] ) {
@@ -1679,7 +1675,7 @@ class ExclusiveSliderItem extends Widget_Base {
 								        $target .= ' rel= nofollow ';
 								    }
 								    echo '<div class="exad-slider-btn">';
-								        // echo '<a class="animated slideInUp" data-wow-duration="1.5s" data-wow-delay=".5s" '.$href.esc_attr($target).'>'.esc_html($each_slide['exad_slider_button_text']).'</a>';
+								        echo '<a data-animation-in="fadeInDown" data-delay-in=".4" data-duration-in=".4" data-animation-out="fadeOutDown" data-delay-out="3" data-duration-out=".5" '.$href.esc_attr($target).'>'.esc_html($each_slide['exad_slider_button_text']).'</a>';
 								    echo '</div>';
 								endif;
 
