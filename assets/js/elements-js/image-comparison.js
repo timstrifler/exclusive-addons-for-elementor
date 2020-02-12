e6343d99e88139a8ea974071da0d5a7a89146544
fahim100 <fahimahmmed100@gmail.com>
2020-02-12 15:14:33 +0600

    Updated image comparison handler and lavel style.

--- assets/js/elements-js/image-comparison.js
+++ assets/js/elements-js/image-comparison.js
@@ -6,7 +6,10 @@ var exclusiveImageComparison = function($scope, $) {
     exadBeforeLabel      = imageComparison.data( 'exad-before_label' ),
     exadAfterLabel       = imageComparison.data( 'exad-after_label' ),
     exadDefaultOffsetPct = imageComparison.data( 'exad-default_offset_pct' ),
-    exadNoOverlay        = imageComparison.data( 'exad-no_overlay' );
+    exadNoOverlay        = imageComparison.data( 'exad-no_overlay' ),
+    exadMoveSliderOnHover        = imageComparison.data( 'exad-move_slider_on_hover' ),
+    exadMoveWithHandleOnly        = imageComparison.data( 'exad-move_with_handle_only' ),
+    exadClickToMove        = imageComparison.data( 'exad-click_to_move' );
         
     if ( $.isFunction($.fn.twentytwenty) ) {    
         imageComparison.twentytwenty({
@@ -14,7 +17,10 @@ var exclusiveImageComparison = function($scope, $) {
             before_label: exadBeforeLabel,
             after_label: exadAfterLabel,
             default_offset_pct: exadDefaultOffsetPct,
-            no_overlay: exadNoOverlay
+            no_overlay: exadNoOverlay,
+            move_slider_on_hover: exadMoveSliderOnHover,
+            move_with_handle_only: exadMoveWithHandleOnly,
+            click_to_move: exadClickToMove
         } );
     }
 }
