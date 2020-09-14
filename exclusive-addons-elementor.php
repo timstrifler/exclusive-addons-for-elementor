4f971fea2372bcb8f6b7fd4299741e063ea15d3b
Muntasir Mahmud Aumio <secretspeed.aumz@gmail.com>
2020-09-14 19:18:11 +0600

    Elementor not activated issues resolved.

--- exclusive-addons-elementor.php
+++ exclusive-addons-elementor.php
@@ -33,11 +33,99 @@ if ( ! defined( 'MINIMUM_PHP_VERSION' ) ) define( 'MINIMUM_PHP_VERSION', '5.4' )
  */	
 function exad_initiate_plugin() {
 
+	// Check if Elementor installed and activated
+	if ( ! did_action( 'elementor/loaded' ) ) {
+		add_action( 'admin_notices', 'exad_admin_notice_missing_elementor' );
+		return;
+	}
+
+	// Check for required Elementor version
+	if ( ! version_compare( ELEMENTOR_VERSION, MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+		add_action( 'admin_notices', 'exad_admin_notice_minimum_elementor_version' );
+		return;
+	}
+
+	// Check for required PHP version
+	if ( version_compare( PHP_VERSION, MINIMUM_PHP_VERSION, '<' ) ) {
+		add_action( 'admin_notices', 'exad_admin_notice_minimum_php_version' );
+		return;
+	}
+
 	require_once EXAD_PATH . 'base.php';
 	\ExclusiveAddons\Elementor\Base::instance();
 } 
 add_action( 'plugins_loaded', 'exad_initiate_plugin' );
 
+/**
+ * Admin notice
+ * Warning when the site doesn't have Elementor installed or activated.
+ *
+ * @since 1.0.0
+ *
+ * @access public
+ */
+function exad_admin_notice_missing_elementor() {
+
+	$message = sprintf(
+        __( '%1$s requires %2$s to be installed and activated to function properly. %3$s', 'exclusive-addons-elementor' ),
+        '<strong>' . __( 'Exclusive Addons Elementor', 'exclusive-addons-elementor' ) . '</strong>',
+        '<strong>' . __( 'Elementor', 'exclusive-addons-elementor' ) . '</strong>',
+        '<a href="' . esc_url( admin_url( 'plugin-install.php?s=Elementor&tab=search&type=term' ) ) . '">' . __( 'Please click here to install/activate Elementor', 'exclusive-addons-elementor' ) . '</a>'
+    );
+
+    printf( '<div class="notice notice-warning is-dismissible"><p style="padding: 5px 0">%1$s</p></div>', $message );
+
+}
+
+/**
+ * Admin notice
+ *
+ * Warning when the site doesn't have a minimum required Elementor version.
+ *
+ * @since 1.0.0
+ *
+ * @access public
+ */
+function exad_admin_notice_minimum_elementor_version() {
+
+	if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+	$message = sprintf(
+		/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+		esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'exclusive-addons-elementor' ),
+		'<strong>' . esc_html__( 'Exclusive Addons Elementor', 'exclusive-addons-elementor' ) . '</strong>',
+		'<strong>' . esc_html__( 'Elementor', 'exclusive-addons-elementor' ) . '</strong>',
+		MINIMUM_ELEMENTOR_VERSION
+	);
+
+	printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+}
+
+/**
+ * Admin notice
+ *
+ * Warning when the site doesn't have a minimum required PHP version.
+ *
+ * @since 1.0.0
+ *
+ * @access public
+ */
+function exad_admin_notice_minimum_php_version() {
+
+	if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+	$message = sprintf(
+		/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+		esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'exclusive-addons-elementor' ),
+		'<strong>' . esc_html__( 'Exclusive Addons Elementor', 'exclusive-addons-elementor' ) . '</strong>',
+		'<strong>' . esc_html__( 'PHP', 'exclusive-addons-elementor' ) . '</strong>',
+		MINIMUM_PHP_VERSION
+	);
+
+	printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+}
 
 /**
  * 
