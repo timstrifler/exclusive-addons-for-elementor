edf4939bfd4ebb15452b40ed73aa9798ac62c54a
fahim100 <fahimahmmed100@gmail.com>
2021-04-21 20:41:18 +0600

    fixed icon folder issue.

--- extensions/exclusive-icon-manager.php
+++ extensions/exclusive-icon-manager.php
@@ -13,39 +13,39 @@ class Icons_Manager {
         $tabs['feather-icons'] = [
             'name' => 'feather-icons',
             'label' => __( 'Feather Icons', 'exclusive-addons-elementor' ),
-            'url' => EXAD_ASSETS_URL . 'fonts/style.css',
-            'enqueue' => [ EXAD_ASSETS_URL . 'fonts/style.css' ],
+            'url' => EXAD_ASSETS_URL . 'fonts/feather-icon/feather-icon-style.min.css',
+            'enqueue' => [ EXAD_ASSETS_URL . 'fonts/feather-icon/feather-icon-style.min.css' ],
             'prefix' => 'icon-',
             'displayPrefix' => 'feather',
             'labelIcon' => 'exad exad-logo feather icon-feather exad-font-manager',
             'ver' => EXAD_PLUGIN_VERSION,
-            'fetchJson' => EXAD_ASSETS_URL . 'fonts/exclusive-icons.js?v=' . EXAD_PLUGIN_VERSION,
+            'fetchJson' => EXAD_ASSETS_URL . 'fonts/feather-icon/exclusive-icons.js?v=' . EXAD_PLUGIN_VERSION,
             'native' => false,
         ];
 
         $tabs['remix-icons'] = [
             'name' => 'remix-icons',
             'label' => __( 'Remix Icons', 'exclusive-addons-elementor' ),
-            'url' => EXAD_ASSETS_URL . 'fonts/remixicon.css',
-            'enqueue' => [ EXAD_ASSETS_URL . 'fonts/remixicon.css' ],
+            'url' => EXAD_ASSETS_URL . 'fonts/remix-icon/remixicon.min.css',
+            'enqueue' => [ EXAD_ASSETS_URL . 'fonts/remix-icon/remixicon.min.css' ],
             'prefix' => 'ri-',
             'displayPrefix' => 'remixicon',
             'labelIcon' => 'exad exad-logo remixicon ri-remixicon-fill exad-font-manager',
             'ver' => EXAD_PLUGIN_VERSION,
-            'fetchJson' => EXAD_ASSETS_URL . 'fonts/remix-icon.js?v=' . EXAD_PLUGIN_VERSION,
+            'fetchJson' => EXAD_ASSETS_URL . 'fonts/remix-icon/remix-icon.js?v=' . EXAD_PLUGIN_VERSION,
             'native' => false,
         ];
 
         $tabs['teeny-icons'] = [
             'name' => 'teeny-icons',
             'label' => __( 'Teeny Icons', 'exclusive-addons-elementor' ),
-            'url' => EXAD_ASSETS_URL . 'fonts/teeny-icon-style.css',
-            'enqueue' => [ EXAD_ASSETS_URL . 'fonts/teeny-icon-style.css' ],
+            'url' => EXAD_ASSETS_URL . 'fonts/teeny-icon/teeny-icon-style.min.css',
+            'enqueue' => [ EXAD_ASSETS_URL . 'fonts/teeny-icon/teeny-icon-style.min.css' ],
             'prefix' => 'ti-',
             'displayPrefix' => 'teenyicon',
             'labelIcon' => 'exad exad-logo teenyicon ti-mood-laugh exad-font-manager',
             'ver' => EXAD_PLUGIN_VERSION,
-            'fetchJson' => EXAD_ASSETS_URL . 'fonts/teeny-icon.js?v=' . EXAD_PLUGIN_VERSION,
+            'fetchJson' => EXAD_ASSETS_URL . 'fonts/teeny-icon/teeny-icon.js?v=' . EXAD_PLUGIN_VERSION,
             'native' => false,
         ];
         return $tabs;
