9921aff7f18dcb697195862c04e3398a39a7eb01
Muntasir Mahmud Aumio <secretspeed.aumz@gmail.com>
2021-04-27 13:53:28 +0600

    Added Container Link

--- assets/js/exad-scripts.js
+++ assets/js/exad-scripts.js
@@ -133,6 +133,36 @@ var exclusiveButton = function ( $scope, $ ) {
 
 // exclusive Button script ends
 
+
+// Container Link JS started
+
+$('body').on('click.onWrapperLink', '[data-exad-element-link]', function() {
+    var $wrapper = $(this),
+        data     = $wrapper.data('exad-element-link'),
+        id       = $wrapper.data('id'),
+        anchor   = document.createElement('a'),
+        anchorReal,
+        timeout;
+
+    anchor.id            = 'exad-container-link-' + id;
+    anchor.href          = data.url;
+    anchor.target        = data.is_external ? '_blank' : '_self';
+    anchor.rel           = data.nofollow ? 'nofollow noreferer' : '';
+    anchor.style.display = 'none';
+
+    document.body.appendChild(anchor);
+
+    anchorReal = document.getElementById(anchor.id);
+    anchorReal.click();
+
+    timeout = setTimeout(function() {
+        document.body.removeChild(anchorReal);
+        clearTimeout(timeout);
+    });
+});
+
+// Container Link JS end
+
 // Corona script starts
 var exclusiveCorona = function ($scope, $) {
 	var exadCoronaWrapper = $scope.find('.exad-corona').eq(0);
