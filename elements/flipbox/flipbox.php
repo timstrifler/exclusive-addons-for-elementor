961b286cd7745a78fa6d30a6a79a022ef40fad0d
madjennsy <madjennsy@users.noreply.github.com>
2023-09-19 11:46:05 -0500

    flipbox: Fix PHP warning where there's null value on exad_flipbox_button_link

--- elements/flipbox/flipbox.php
+++ elements/flipbox/flipbox.php
@@ -1243,13 +1243,13 @@ class Flipbox extends Widget_Base {
 	    $this->add_render_attribute( 'exad_flipbox_button_link', 'class', 'exad-flip-box-back-action' );
 		$this->add_inline_editing_attributes( 'exad_flipbox_button_text', 'none' );
 
-		if( $settings['exad_flipbox_button_link']['url'] ) {
+		if( isset( $settings['exad_flipbox_button_link']['url'] ) ) {
             $this->add_render_attribute( 'exad_flipbox_button_link', 'href', esc_url( $settings['exad_flipbox_button_link']['url'] ) );
         }
-        if( $settings['exad_flipbox_button_link']['is_external'] ) {
+        if( isset( $settings['exad_flipbox_button_link']['is_external'] ) ) {
             $this->add_render_attribute( 'exad_flipbox_button_link', 'target', '_blank' );
         }
-        if( $settings['exad_flipbox_button_link']['nofollow'] ) {
+        if( isset( $settings['exad_flipbox_button_link']['nofollow'] ) ) {
             $this->add_render_attribute( 'exad_flipbox_button_link', 'rel', 'nofollow' );
         }
 		?>
