7a95801398564825e42fa65e92274665a4537a38
devjennz <7797492+devjennz@users.noreply.github.com>
2025-04-21 16:22:42 -0300

    Bump version 2.7.9

--- exclusive-addons-elementor.php
+++ exclusive-addons-elementor.php
@@ -3,7 +3,7 @@
  * Plugin Name: Exclusive Addons Elementor
  * Plugin URI: https://exclusiveaddons.com/
  * Description: Packed with a bunch of Exclusively designed widgets for Elementor with all the customizations you ever imagined.
- * Version: 2.7.8
+ * Version: 2.7.9
  * Author: Exclusive Addons
  * Author URI: https://exclusiveaddons.com
  * Elementor tested up to: 99
@@ -24,7 +24,7 @@ if ( ! defined( 'EXAD_EXTENSIONS' ) ) define( 'EXAD_EXTENSIONS', plugin_dir_path
 if ( ! defined( 'EXAD_TEMPLATES' ) ) define( 'EXAD_TEMPLATES', EXAD_PATH . 'includes/template-parts/' );
 if ( ! defined( 'EXAD_URL' ) ) define( 'EXAD_URL', plugins_url( '/', __FILE__ ) );
 if ( ! defined( 'EXAD_ASSETS_URL' ) ) define( 'EXAD_ASSETS_URL', EXAD_URL . 'assets/' );
-if ( ! defined( 'EXAD_PLUGIN_VERSION' ) ) define( 'EXAD_PLUGIN_VERSION', '2.7.8' );
+if ( ! defined( 'EXAD_PLUGIN_VERSION' ) ) define( 'EXAD_PLUGIN_VERSION', '2.7.9' );
 if ( ! defined( 'MINIMUM_ELEMENTOR_VERSION' ) ) define( 'MINIMUM_ELEMENTOR_VERSION', '2.0.0' );
 if ( ! defined( 'MINIMUM_PHP_VERSION' ) ) define( 'MINIMUM_PHP_VERSION', '7.0' );
 
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: exclusiveaddons, timstrifler, devjenndivilife, mmaumio, atiquehasan, zabid, fahimahmmed100
 Tags: elementor, elementor Addons, elementor widgets, woocommerce builder, ultimate Addons
 Requires at least: 4.6
-Tested up to: 6.7.2
+Tested up to: 6.8
 Stable tag: 2.7.8
 License: GPLv3
 License URI: https://opensource.org/licenses/GPL-3.0
@@ -273,6 +273,9 @@ No, It won't break your existing elements. We put most of our efforts into ensur
 
 
 == Changelog ==
+= 2.7.9 - 21/04/2025 =
+
+
 = 2.7.8 - 04/03/2025 =
 - Fix frontend PHP notice issue using Heading widget
 
